c4169172a9adbe97f5e265f302a71fd26ebb6055
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2023-09-19 12:26:32 +0300

    make 1password plugins portable between my devices

--- fish/config.fish
+++ fish/config.fish
@@ -16,7 +16,12 @@ alias bubu="brew update; and brew outdated; and brew upgrade; and brew cleanup"
 
 if status is-interactive 
 	# 1Password plugins
-	source $HOME/.op/plugins.sh
+	if [ -e "$HOME/.op/plugins.sh" ] 
+		source $HOME/.op/plugins.sh
+	else if [ -e "$HOME/.config/op/plugins.sh" ]
+		source $HOME/.config/op/plugins.sh
+	end
+
 	eval (/opt/homebrew/bin/brew shellenv)
 end
 
