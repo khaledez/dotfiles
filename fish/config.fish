1ec187bb879f37801dc4fd1fc7a26fa0f97c0a77
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2023-09-27 11:51:35 +0300

    remove 1password plugin

--- fish/config.fish
+++ fish/config.fish
@@ -16,12 +16,6 @@ alias bubu="brew update; and brew outdated; and brew upgrade; and brew cleanup"
 
 if status is-interactive 
 	base16-snazzy
-	# 1Password plugins
-	if [ -e "$HOME/.op/plugins.sh" ] 
-		source $HOME/.op/plugins.sh
-	else if [ -e "$HOME/.config/op/plugins.sh" ]
-		source $HOME/.config/op/plugins.sh
-	end
 
 	eval (/opt/homebrew/bin/brew shellenv)
 end
